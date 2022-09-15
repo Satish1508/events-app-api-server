8b08056d1052d11fee28c205b76cf8e808501683
satish1508 <sgopinathkodag@deloitte.com>
2022-09-15 04:24:37 +0000

    some message

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Satish G. Kodag Ran';
+    team = 'Satish G. Kodag';
     version = '1.0.0';
 }
 module.exports = AppConfig;
